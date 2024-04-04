e49a3f5c87fd044947bc97bccce7161d28585fab
Nien-Chi Melody Yu <m4745melody0706@gmail.com>
2024-04-04 16:26:46 -0700

    first commit

---
+++ src/main/java/com/example/platstyle/PlatstyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.platstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatstyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatstyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/platstyle/PlatstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatstyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
